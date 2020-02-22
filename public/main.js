35a52c6e27122f014a14117f9b50ade451d0684a
shailen <shailen@lumkani.com>
2020-02-22 16:03:57 +0200

    Made dark mode persist

--- public/main.js
+++ public/main.js
@@ -2,10 +2,28 @@ const root = document.documentElement;
 const buttons = document.querySelectorAll('button');
 const supportsCssVar = CSS.supports('color', 'var(--test)');
 
-let dark = false;
+let dark = JSON.parse(localStorage.getItem('app')).dark || false
 
 const blurButtons = () => {
   Array.from(buttons).forEach(instance => instance.blur())
+}
+
+if (dark) {
+  root.style.setProperty('--background', '#121212');
+  root.style.setProperty('--base', 'white');
+  root.style.setProperty('--text', '#CDCCCD');
+  root.style.setProperty('--accent', '#3F3F3F');
+  root.style.setProperty('--surface', '#363636');
+  Array.from(buttons).forEach(instance => {
+    instance.classList.add('active');
+    instance.disabled = false;
+  });
+} else {
+  root.style.setProperty('--background', 'white');
+  root.style.setProperty('--base', 'black');
+  root.style.setProperty('--text', '#3B454E');
+  root.style.setProperty('--accent', '#EEEEEE');
+  root.style.setProperty('--surface', '#E0E0E0');
 }
 
 const toggleTheme = () => {
@@ -16,6 +34,9 @@ const toggleTheme = () => {
     root.style.setProperty('--accent', '#EEEEEE');
     root.style.setProperty('--surface', '#E0E0E0');
     dark = false;
+    localStorage.setItem('app', JSON.stringify({
+      dark: false,
+    }))
     buttons.innerText = 'Toggle Dark Mode';
     blurButtons()
     return;
@@ -27,6 +48,9 @@ const toggleTheme = () => {
   root.style.setProperty('--accent', '#3F3F3F');
   root.style.setProperty('--surface', '#363636');
   dark = true;
+  localStorage.setItem('app', JSON.stringify({
+    dark: true,
+  }))
   buttons.innerText = 'Toggle Light Mode';
   blurButtons()
   return;
