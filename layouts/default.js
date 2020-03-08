ab0073fc63d7f2fcc49fbdc367b94a6fdf44281d
shailen <shailen@lumkani.com>
2020-03-08 11:01:16 +0200

    Added constitution to cms

--- layouts/default.js
+++ layouts/default.js
@@ -5,14 +5,15 @@ module.exports = ({ content, head }) => html `
   <html>
     <head>
       $${head}
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Muli&display=swap">
       <link rel="stylesheet" href="/public/main.css">
-      <script src="/public/darkmode.js"></script>
     </head>
     <body>
       <div class="Layout">
         $${content}
       </div>
     </body>
+    <script src="/public/darkmode.js"></script>
     <script src="/public/main.js" defer async></script>
   </html>
 `
