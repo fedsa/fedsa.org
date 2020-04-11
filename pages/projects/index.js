48e3026d1d23af3565b79853ae2a5a270698c257
Shailen Naidoo <naidooshailen648@gmail.com>
2020-04-11 14:00:56 +0200

    Moved project styles to main.css

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -2,7 +2,7 @@ const html = require('html-template-tag')
 const { projects } = require('../../public/data/projects.json')
 
 const ProjectCard = ({ title, description, link = false }) => html`
-  <a class="card" ${link ? `href=${link}` : ''}>
+  <a class="Card" ${link ? `href=${link}` : ''}>
     <h2>${title}</h2>
     <p>${description}</p>
   </a>
@@ -13,34 +13,6 @@ const page = () =>  html`
     <div class="Layout-row Smaller-width">
       ${projects.map(ProjectCard)}
     </div>
-    <style>
-      .card {
-        padding: 5px 20px 10px 20px;
-        border: 1px solid var(--accent);
-        border-radius: 4px;
-        width: 100%;
-        margin: 7px 0px;
-        cursor: pointer;
-        text-decoration: none;
-        color: var(--base)
-      }
-
-      .card:nth-child(3n+1) {
-        border-left: 3px solid #ecb31e;
-      }
-
-      .card:nth-child(3n+2) {
-        border-left: 3px solid #105ea9;
-      }
-
-      .card:nth-child(3n+3) {
-        border-left: 3px solid #eb5022;
-      }
-
-      .card:hover {
-        box-shadow: rgba(116, 129, 141, 0.1) 0px 3px 8px;
-      }
-    </style>
   </main>
 `
 
