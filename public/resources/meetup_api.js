944f9002808efbbb1d0bf8702459add20d9128e7
jordanhailey <jordan.m.hailey@gmail.com>
2021-07-10 23:49:18 -0700

    init loading and showing meetup data

--- public/resources/meetup_api.js
+++ public/resources/meetup_api.js
@@ -1,23 +1,31 @@
 let events;
 const container = document.getElementById("meetup"),
-  placeholder = document.getElementById("meetup_placeholder");
-
+  placeholder = document.getElementById("meetup_placeholder"),
+  placeholderLink = placeholder.querySelector('a'),
+  placeholderStatus = placeholder.querySelector('#meetup_placeholder_status');
 
 /** Build out event cards and add to DOM */
-function createEventCard() {
-
+function createEventCard({title}) {
+  let cntnr = document.createElement("div");
+  let content = document.createTextNode(title);
+  cntnr.appendChild(content);
+  container.insertBefore(cntnr,placeholder);
 }
 
 (
   function loadingIndicator() {
-    placeholder.innerText = "Loading..."
+    placeholderStatus.innerText = "(fetching...)";
     let interval = setInterval(()=>{
-      if (!events) placeholder.innerText += ".";
-      else clearInterval(interval)
+      if (!events) placeholderStatus.innerText = placeholderStatus.innerText.replace(/([)])$/,".)");
+      else clearInterval(interval);
     },800)
     setTimeout(()=>{
-      if (interval !== undefined) clearInterval(interval)
-    },10000)
+      if (interval !== undefined) clearInterval(interval);
+      if (!events) {
+        placeholderStatus.innerText = "(unable to fetch)";
+        setTimeout(()=>{placeholder.removeChild(placeholderStatus)},2000);
+      }
+    },4200)
   }
 )();
 
@@ -28,6 +36,10 @@ function createEventCard() {
       .then(res=>res.json())
       .then(oldToNewArr=>oldToNewArr.reverse());
     events = meetupEvents;
-    placeholder.innerText = JSON.stringify(events,null,2)
+    console.log({events});
+    placeholder.removeChild(placeholderStatus)
+    events.forEach(e => {
+      createEventCard({title:e.name})
+    });
   }
 )();
