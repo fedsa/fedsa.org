a5336cc678486cecef0677fbb0a45f724aaa643a
Schalk Venter <venterschalk@gmail.com>
2022-11-26 14:46:27 +0200

    Revert to main pupetteer

--- netlify/functions/meetup.js
+++ netlify/functions/meetup.js
@@ -1,4 +1,4 @@
-const puppeteer = require("puppeteer-core");
+const puppeteer = require("puppeteer");
 
 exports.handler = async function (event, context) {
   const browser = await puppeteer.launch({ headless: false });
